1bd413d93c9ffe1a7d30180256247d577bd5fc89
Clarence Stark <3038736583@qq.com>
2023-10-04 02:52:35 +0800

    third submission of lab2 IntList

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -80,6 +80,9 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
+        if (A == null) {
+            return B;
+        }
         IntList C = A;
         while (C.rest != null) {
             C = C.rest;
@@ -93,6 +96,9 @@ public class IntList {
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
+        if (A == null) {
+            return B;
+        }
         IntList C = new IntList(A.first, null);
         IntList D = C;
         A = A.rest;
