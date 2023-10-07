8753115c2d1684ec02192dfb3203fb88e7dbda2b
Clarence Stark <3038736583@qq.com>
2023-10-08 04:43:46 +0800

    first commit for proj1b

--- proj1b/LinkedListDeque.java
+++ proj1b/LinkedListDeque.java
@@ -1,2 +1,137 @@
-package PACKAGE_NAME;public class LinkedListDeque {
+public class LinkedListDeque<T> implements Deque<T> {
+    //Adds an item of type T to the front of the deque
+    private int size = 0;
+
+    private class Tnode<T>{
+        T item;
+        Tnode next;
+        Tnode last;
+        private Tnode(T i, Tnode l, Tnode n){
+            item = i;
+            last = l;
+            next = n;
+        }
+    }
+    private Tnode fakeFirst;
+    private Tnode fakeLast;
+    private T passer;
+
+    private int index = 0;
+    public LinkedListDeque() {
+        fakeFirst = new Tnode(passer, null, null);
+        fakeLast = new Tnode(passer, null, null);
+        fakeFirst.next = fakeLast;
+        fakeLast.last = fakeFirst;
+    }
+    @Override
+    public void addFirst(T item){
+        Tnode t  = new Tnode(item, fakeFirst, fakeFirst.next);
+        fakeFirst.next.last = t;
+        fakeFirst.next = t;
+        size += 1;
+    }
+    //Adds an item of type T to the back of the deque
+    @Override
+    public void addLast(T item){
+        Tnode t = new Tnode(item, fakeLast.last, fakeLast);
+        fakeLast.last.next = t;
+        fakeLast.last = t;
+        size += 1;
+    }
+    //Returns true if deque is empty, false otherwise
+    @Override
+    public boolean isEmpty(){
+        if (fakeFirst.next == fakeLast) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+    //Returns the number of items in the deque.
+    @Override
+    public int size(){
+        return size;
+    }
+    //Prints the items in the deque from first to last, separated by a space
+    @Override
+    public void printDeque(){
+        Tnode temp = fakeFirst;
+        while (temp.next != fakeLast){
+            System.out.print(temp.next.item);
+            if (temp.next.next != fakeLast) {
+                System.out.print(' ');
+            }
+            temp = temp.next;
+        }
+    }
+    //Removes and returns the item at rhe front of the deque. If no such item exists, returns null.
+    @Override
+    public T removeFirst(){
+        if (this.isEmpty() == true){
+            return null;
+        } else {
+            size -= 1;
+            T item = (T) fakeFirst.next.item;
+            fakeFirst.next = fakeFirst.next.next;
+            fakeFirst.next.last = fakeFirst;
+            return item;
+        }
+    }
+    //Removes and returns the item at the front of the deque. If no such item exists, returns null.
+    @Override
+    public T removeLast(){
+        if (this.isEmpty() == true){
+            return null;
+        } else {
+            size -= 1;
+            T item = (T) fakeLast.last.item;
+            fakeLast.last = fakeLast.last.last;
+            fakeLast.last.next = fakeLast;
+            return item;
+        }
+    }
+    //Gets the item at the given index, where 0 is the front, 1 is the next item. and so forth. If no such item exists, returns null. Must not alter the deque!
+    @Override
+    public T get(int index){
+        Tnode temp = fakeFirst.next;
+        if (temp == fakeLast){
+            return null;
+        }
+        for (int i = 0; i < index; i++){
+            temp = temp.next;
+            if (temp == fakeLast){
+                return null;
+            }
+        }
+        return (T)temp.item;
+    }
+    private T getRecursiveHelper(Tnode t, int i){
+        if (t == fakeLast){
+            return null;
+        }
+        if (i == index){
+            return (T)t.item;
+        } else{
+            index += 1;
+            return (T)getRecursiveHelper(t.next, i);
+        }
+    }
+    @Override
+    public  T getRecursive(int index){
+        return (T)getRecursiveHelper(fakeFirst.next, index);
+    }
+    /*public static void main(String[] args){
+        LinkedListDeque<Integer> l = new LinkedListDeque<>();
+        l.addFirst(10);
+        l.removeFirst();
+        l.addFirst(8);
+        l.addLast(3);
+        l.addFirst(2);
+        l.addLast(5);
+        l.removeLast();
+        l.removeFirst();
+        l.removeFirst();
+        l.removeFirst();
+        System.out.println(l.isEmpty());
+    }*/
 }
